0869c08e01a26e69d256da58d7eb05a1701fc038
Charlie Rolando <106483372+charlierolando@users.noreply.github.com>
2023-11-21 16:56:47 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,7 +190,7 @@
   /**
    * Porfolio isotope and filter
    */
-  /**
+  
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -215,32 +215,32 @@
     }
 
   });
-  */
+
 
   /**
    * Initiate portfolio lightbox 
    */
-  /**
+  
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
-  */
+
 
   /**
    * Initiate portfolio details lightbox 
    */
-  /**
+  
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
-  */
+  
 
   /**
    * Portfolio details slider
    */
-  /**
+  
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -254,7 +254,7 @@
       clickable: true
     }
   });
-  */
+  
 
   /**
    * Initiate Pure Counter 
